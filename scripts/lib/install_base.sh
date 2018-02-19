d8b4939d8ac1765ba5f37d0977abd390a8843991
Patrekur M Magnusson <pmm1@hi.is>
2018-02-19 02:27:11 +0000

    Fixed .sh script bug

--- scripts/lib/install_base.sh
+++ scripts/lib/install_base.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y curl bzip2 build-essential g++ python git libjpeg-dev libpango1.0-dev libgif-dev fonts-cantarell ttf-ubuntu-font-family
+apt-get install -y curl bzip2 build-essential g++ python git libjpeg-dev libpango1.0-dev libgif-dev fonts-cantarell fonts-lato
+
+echo "Updating font-cache..."
+fc-cache -f > /dev/null
