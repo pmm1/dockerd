d33a6c19a71a676f72d861bb8d2777af1f1b9a67
Patrekur M Magnusson <pmm1@hi.is>
2018-02-19 02:10:35 +0000

    added google fonts to server, testing this stuff

--- scripts/lib/install_base.sh
+++ scripts/lib/install_base.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y curl bzip2 build-essential g++ python git libjpeg-dev libpango1.0-dev libgif-dev
+apt-get install -y curl bzip2 build-essential g++ python git libjpeg-dev libpango1.0-dev libgif-dev fonts-cantarell ttf-ubuntu-font-family
---
+++ scripts/lib/install_google_fonts.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Written by: Keefer Rourke <https://krourke.org>
+# Based on AUR package <https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=ttf-google-fonts-git>
+
+# dependancies: fonts-cantarell, ttf-ubuntu-font-family, git
+set -e
+srcdir="/tmp/google-fonts"
+pkgdir="/usr/share/fonts/truetype/google-fonts"
+giturl="git://github.com/google/fonts.git"
+
+mkdir $srcdir
+cd $srcdir
+echo "Cloning Git repository..."
+git clone $giturl
+
+echo "Installing fonts..."
+sudo mkdir -p $pkgdir
+sudo find $srcdir -type f -name "*.ttf" -exec install -Dm644 {} $pkgdir \;
+
+echo "Cleaning up..."
+sudo find $pkgdir -type f -name "Cantarell-*.tff" -delete \;
+sudo find $pkgdir -type f -name "Ubuntu-*.tff" -delete \;
+
+# provides roboto
+apt-get --purge remove fonts-roboto
+
+echo "Updating font-cache..."
+fc-cache -f > /dev/null
+
+echo "Done!"
